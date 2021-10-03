f7e4fbb42ff0c2407fcd6d04dc429ac6db42a272
charleskylereyes16@gmail.com <charleskylereyes16@gmail.com>
2021-10-03 14:29:30 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+import json
+import pprint
+
+
+response = requests.get('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
+
+for data in response.json()['items']:
+    if data['answer_count'] == 0:
+        print(data['title'])
+        print(data['link'])
+    else:
+        print("skipped")
+    print()
